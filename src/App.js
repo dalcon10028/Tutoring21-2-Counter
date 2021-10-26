a5662ceddb61b430df4cffce66a81ad747792494
gudwns1458 <70959328+gudwns1458@users.noreply.github.com>
2021-10-26 22:10:21 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App {
     }
 
     setState(nextState) {
-        this.state = { ...this.state, nextState };
+        this.state = { ...this.state, ...nextState };
         this.render();
     }
 
